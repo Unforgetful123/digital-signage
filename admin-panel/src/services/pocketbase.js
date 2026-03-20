7e74481feb2091df91e1942585fa524f56d8a7f4
Unforgetful123 <johnmantraj@gmail.com>
2026-03-20 17:26:29 +0530

    setup IP change

--- admin-panel/src/services/pocketbase.js
+++ admin-panel/src/services/pocketbase.js
@@ -1,7 +1,14 @@
-﻿// src/services/pocketbase.js
-import PocketBase from 'pocketbase';
+﻿import PocketBase from 'pocketbase';
 
-const serverIp = window.location.hostname; 
-const pb = new PocketBase('https://postilioned-ema-nebulously.ngrok-free.dev');
+const url = localStorage.getItem('server_url') || "https://postilioned-ema-nebulously.ngrok-free.dev";
+const pb = new PocketBase(url);
+
+// ADD THIS BLOCK TO BYPASS NGROK'S WARNING PAGE
+pb.beforeSend = function (url, reqOpts) {
+    reqOpts.headers = Object.assign({}, reqOpts.headers, {
+        'ngrok-skip-browser-warning': 'true'
+    });
+    return { url, reqOpts };
+};
 
 export default pb;
